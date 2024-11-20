7a8c4f00774937ea55a4cc0907e8b93b5c0ab618
Lalithtuppad <lalithtuppad@gmail.com>
2024-11-20 22:08:50 +0530

    Deleted TCases Folder

--- tests/TCases/tests/cases/case7.spec.js
+++
@@ -1,8 +0,0 @@
-const{test,page, expect}=require('@playwright/test');
-test('Verifying the Test cases page',async({page})=>{
-    await page.goto('/');
-    await expect(page.getByText('Category')).toBeVisible();
-    await page.getByRole('listitem').filter({hasText:" Test Cases"}).click();
-    await expect(page.getByText('Below is the list of test Cases for you to practice the Automation. Click on the scenario for detailed Test Steps:')).toBeVisible();
-    await page.waitForTimeout(5000);
-})
